0804348e05412aaa6eb75a903ed6cb06df3261f8
NewGhost2020 <i2972347@icloud.com>
2020-04-11 12:29:20 -0700

    Upload files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// -------------------------burger-menu--------------------------
+let i = false;
+document.querySelector('.burger-main').onclick = () => {
+    i = !i;
+    if (i) {
+        document.querySelector('.navbar-nav').classList.remove('no-show');
+    }
+    else {
+        document.querySelector('.navbar-nav').classList.add('no-show');
+    }
+}
+document.querySelector('.burger-toggle').onclick = () => {
+    document.querySelector('.navbar-nav').classList.remove('no-show');
+}
+
+
